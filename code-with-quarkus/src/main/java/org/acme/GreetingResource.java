2b054db07e4090a62e4d90dad94157892fd4e59f
JayGhiya <ghiya6548@gmail.com>
2024-04-08 17:51:50 +0530

    feat: generate openapi schema for classes endpoint of archguard. Ready Accept it on localhost:8080/class-items

--- code-with-quarkus/src/main/java/org/acme/GreetingResource.java
+++ code-with-quarkus/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,44 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import io.quarkiverse.openapi.generator.annotations.GeneratedMethod;
+import io.quarkiverse.openapi.generator.annotations.GeneratedParam;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.openapi.quarkus.archguard_yaml.api.DefaultApi;
+import org.openapi.quarkus.archguard_yaml.model.CodeDataStruct;
 
-@Path("/hello")
-public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
-    }
+@ApplicationScoped
+public class GreetingResource implements DefaultApi {
+
+    private String systemId;
+    private String language;
+    private String path;
+    private String repoId;
+    private CodeDataStruct codeDataStruct;
+
+    @Override
+    @POST
+    @Consumes({"application/json"})
+    @GeneratedMethod("")
+    public jakarta.ws.rs.core.Response classItemsPost(
+  @GeneratedParam("systemId") @PathParam("systemId") String systemId
+, @GeneratedParam("language") @QueryParam("language") String language
+, @GeneratedParam("path") @QueryParam("path") String path
+, @GeneratedParam("repoId") @QueryParam("repoId") String repoId
+, CodeDataStruct codeDataStruct)
+{
+    this.systemId = systemId;
+    this.language = language;
+    this.path = path;
+    this.repoId = repoId;
+    this.codeDataStruct = codeDataStruct;
+    System.out.println("Saving CodeDataStruct for systemId: " + systemId + ", language: " + language);
+        
+// Assuming the save operation is successful
+    return Response.ok().build();
+}
+    
 }
