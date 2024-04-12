b5e6768e6dfb040d1435b21992119e6a978ee055
JayGhiya <ghiya6548@gmail.com>
2024-04-12 14:47:59 +0530

    feat: api

--- code-with-quarkus/src/main/java/org/acme/ScannerReportingResource.java
+++ code-with-quarkus/src/main/java/org/acme/ScannerReportingResource.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import jakarta.ws.rs.QueryParam;
 import org.acme.unoplat.models.model.CodeDataStruct;
 import org.jboss.logging.Logger;
 
@@ -11,17 +12,17 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/scanner/{systemId}/reporting/class-items")
+@Path("api/scanner/{systemId}/reporting/class-items")
 public class ScannerReportingResource {
     private static final Logger LOG = Logger.getLogger(ScannerReportingResource.class);
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response saveClassItems(@PathParam("systemId") String systemId,@NotNull CodeDataStruct codeDataStruct) {
+    public Response saveClassItems(@PathParam("systemId") String systemId, @QueryParam("language") String language, @QueryParam("path") String path, @NotNull CodeDataStruct codeDataStruct) {
         // Here you would include your logic to process the CodeDataStruct object
         // For example, saving it to a database or performing some analysis
-        LOG.infof("Received class items for system ID %s: %s", systemId, codeDataStruct);
+        LOG.infof("Received class items for system ID %s, Language: %s, Path: %s: %s", systemId, language, path, codeDataStruct);
         // Returning a simple response for demonstration purposes
-        return Response.ok().entity("Data for system ID " + systemId + " received and processed.").build();
+        return Response.ok().entity(String.format("Data for system ID %s, Language: %s, Path: %s received and processed.", systemId, language, path)).build();
     }
 }
