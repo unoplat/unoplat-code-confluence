ab04f7859afd37ae0c7b99e663d377e39d79bc3e
JayGhiya <ghiya6548@gmail.com>
2024-06-14 11:52:29 +0530

    fix: repeatitive attributes in function

--- unoplat-code-confluence/data_models/chapi_unoplat_function.py
+++ unoplat-code-confluence/data_models/chapi_unoplat_function.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List
-from .chapi_unoplat_functioncall import FunctionCall
+
+from data_models.chapi_unoplat_functioncall import FunctionCall
 from .chapi_unoplat_annotation import Annotation
 from .chapi_unoplat_position import Position
 from .chapi_unoplat_fieldmodel import FieldModel
@@ -10,8 +11,6 @@ class Function(BaseModel):
     return_type: Optional[str] = Field(default=None, alias="ReturnType")
     function_calls: List[FunctionCall] = Field(default_factory=list, alias="FunctionCalls")
     annotations: List[Annotation] = Field(default_factory=list, alias="Annotations")
-    position: Optional[Position] = Field(default=None, alias="Position")
-    local_variables: List[FieldModel] = Field(default_factory=list, alias="LocalVariables")
     position: Optional[Position] = Field(default=None, alias="Position")
     local_variables: List[FieldModel] = Field(default_factory=list, alias="LocalVariables")
     body_hash: Optional[int] = Field(default=None, alias="BodyHash")
