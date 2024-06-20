07658b2628c8ea905b6d32af467f709be05ae3cd
JayGhiya <ghiya6548@gmail.com>
2024-06-20 12:22:54 +0530

    feat: fix and enhance models for chapi and for dspy

--- unoplat-code-confluence/data_models/chapi_unoplat_annotation.py
+++ unoplat-code-confluence/data_models/chapi_unoplat_annotation.py
@@ -1,8 +1,10 @@
+from data_models.chapi_unoplat_annotation_key_val import ChapiUnoplatAnnotationKeyVal
 from data_models.chapi_unoplat_position import Position
 from pydantic import BaseModel, Field
 from typing import Optional
 
+from data_models.dspy.dspy_unoplat_fs_annotation_subset import DspyUnoplatAnnotationSubset
 
-class Annotation(BaseModel):
-    name: Optional[str] = Field(default=None, alias="Name")
+
+class Annotation(DspyUnoplatAnnotationSubset):
     position: Optional[Position] = Field(default=None, alias="Position")
--- unoplat-code-confluence/data_models/chapi_unoplat_node.py
+++ unoplat-code-confluence/data_models/chapi_unoplat_node.py
@@ -1,22 +1,26 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List
-from data_models.chapi_unoplat_fieldmodel import FieldModel
+from data_models.chapi_unoplat_annotation import Annotation
+from data_models.chapi_unoplat_class_summary import ClassSummary
+from data_models.chapi_unoplat_class_fieldmodel import ClassFieldModel
 from data_models.chapi_unoplat_import import Import
 from data_models.chapi_unoplat_function import Function
 from data_models.chapi_unoplat_position import Position
+from data_models.dspy.dspy_unoplat_fs_node_subset import DspyUnoplatNodeSubset
 
 
-class Node(BaseModel):
+class Node(DspyUnoplatNodeSubset):
     node_name: Optional[str] = Field(default=None, alias="NodeName")
     type: Optional[str] = Field(default=None, alias="Type")
     file_path: Optional[str] = Field(default=None, alias="FilePath")
     module: Optional[str] = Field(default=None, alias="Module")
     package: Optional[str] = Field(default=None, alias="Package")
     multiple_extend: Optional[bool] = Field(default=None, alias="MultipleExtend")
-    fields: List[FieldModel] = Field(default_factory=list, alias="Fields")
+    fields: List[ClassFieldModel] = Field(default_factory=list, alias="Fields")
     extend: Optional[str] = Field(default=None, alias="Extend")
     imports: List[Import] = Field(default_factory=list, alias="Imports")
     functions: List[Function] = Field(default_factory=list, alias="Functions")
     position: Optional[Position] = Field(default=None, alias="Position")
-    summary: Optional[str] = Field(default=None, alias="Summary")
+    summary: Optional[ClassSummary] = Field(default=None, alias="Summary")
     content: Optional[str] = Field(default=None, alias="Content")
+    annotations: List[Annotation] = Field(default_factory=list, alias="Annotations")
