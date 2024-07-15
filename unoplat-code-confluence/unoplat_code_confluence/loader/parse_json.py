d39ae0680f68c871d3eb9e6e909ac9846f519c3b
JayGhiya <ghiya6548@gmail.com>
2024-07-15 12:00:10 +0530

    feat: drastically improved summarisation with chainofthoughtwithhints

--- unoplat-code-confluence/unoplat_code_confluence/loader/parse_json.py
+++ unoplat-code-confluence/unoplat_code_confluence/loader/parse_json.py
@@ -1,3 +1,4 @@
+import os
 from typing import Dict, List
 from pydantic import ValidationError
 from unoplat_code_confluence.data_models.chapi_unoplat_codebase import UnoplatCodebase
@@ -12,10 +13,11 @@ from loguru import logger
 from unoplat_code_confluence.nodeparser.isummariser import ISummariser
 
 class JsonParser(IParseJson):
-    def parse_json_to_nodes(self, json_data: dict) -> UnoplatCodebase:
+    def parse_json_to_nodes(self, json_data: dict, local_workspace_path: str, programming_language: str) -> UnoplatCodebase:
         """Concrete implementation of the parse_json_to_nodes method."""
         unoplat_codebase = UnoplatCodebase()
         
+    
         unoplat_package = UnoplatPackage()
         
         unoplat_package_dict: Dict[str,List[DspyUnoplatNodeSubset]] = {}
@@ -23,8 +25,13 @@ class JsonParser(IParseJson):
         for item in json_data:
             try:
                 node = Node(**item)
-                if node.type == 'CLASS':
                 
+                # TODO: node package is not present in python. so use node file path and local workspace path to identify package
+                if programming_language.lower() == 'python':
+                    relative_path = os.path.relpath(node.file_path, local_workspace_path)
+                    node.package = os.path.dirname(relative_path).replace(os.path.sep, '.')
+                    node.package = node.package if node.package else 'root'
+                if node.type == 'CLASS':
                     # Creating node subset
                     node_subset = DspyUnoplatNodeSubset(
                     NodeName=node.node_name,
@@ -52,9 +59,9 @@ class JsonParser(IParseJson):
                     node_subset.functions = function_subset_list
                     
                     if node.package in unoplat_package_dict:
-                        print("added package",node.package)
                         unoplat_package_dict[node.package].append(node_subset)
                     else:
+                        logger.debug(f"Identified new package: {node.package}")
                         list_node_subset: List[DspyUnoplatNodeSubset] = [] 
                         list_node_subset.append(node_subset)
                         unoplat_package_dict[node.package] = list_node_subset
