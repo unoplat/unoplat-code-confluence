b8d5c6f094953e76f68a8e5ea45b76290f7a39cf
Milind <milind.12ghiya@gmail.com>
2024-08-13 13:25:43 +0530

    fix site baseurl

--- code-confluence/docusaurus.config.js
+++ code-confluence/docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://unoplat.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/unoplat-code-confluence/code-confluence/production/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'unoplat', // Usually your GitHub org/user name.
+  projectName: 'code-confluence', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
