a783fc36cb503d222e4fa22fcd327bc2ffd102a0
Milind <milind.12ghiya@gmail.com>
2024-08-13 12:56:20 +0530

    add code confluence website

---
+++ code-confluence/src/pages/features.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function features() {
+  return (
+    <Layout title="Features" description="This is features page">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+          This page is under construction
+        </p>
+      </div>
+    </Layout>
+  );
+}
