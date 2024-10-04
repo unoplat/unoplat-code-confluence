75a62eb7eaae7b0e4b6cbc2bcc518e24d0e41a7d
JayGhiya <ghiya6548@gmail.com>
2024-10-04 12:30:08 +0530

    chore: remove redudant build folders

--- docs/assets/js/c141421f.1dc9898d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcode_confluence=self.webpackChunkcode_confluence||[]).push([[957],{936:e=>{e.exports=JSON.parse('{"name":"docusaurus-theme-search-algolia","id":"default"}')}}]);
