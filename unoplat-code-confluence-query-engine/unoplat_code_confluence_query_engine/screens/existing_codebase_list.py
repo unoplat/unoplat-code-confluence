6f24eff27974463cdecf8685e37b3390e31fd343
JayGhiya <ghiya6548@gmail.com>
2024-10-06 10:41:21 +0530

    feat: updated existing codebase design

--- unoplat-code-confluence-query-engine/unoplat_code_confluence_query_engine/screens/existing_codebase_list.py
+++ unoplat-code-confluence-query-engine/unoplat_code_confluence_query_engine/screens/existing_codebase_list.py
@@ -1,31 +1,31 @@
-
 from textual.screen import ModalScreen
 from textual.app import ComposeResult
-from textual.widgets import Button, Label, DirectoryTree
+from textual.widgets import Button, Label, ListView, ListItem, Header, Footer
 from textual.containers import Container
 from processing.query_engine_process import QueryEngineProcess
-import os
-from widgets.json_directory_tree import JSONDirectoryTree
-from textual.widgets import ListView, ListItem
 from textual.binding import Binding
+
 class ExistingCodebaseList(ModalScreen):
     
     BINDINGS = [
-        Binding("ctrl+q", "request_quit", "Quit",show=True,priority=True),
-            ]
+        Binding("ctrl+q", "request_quit", "Quit", show=True, priority=True),
+    ]
     
-    def action_request_quit(self) -> None:
-        self.app.pop_screen()
-
     def __init__(self, query_engine_process: QueryEngineProcess, **kwargs):
         super().__init__(**kwargs)
         self.query_engine_process = query_engine_process
 
     def compose(self) -> ComposeResult:
-            yield Container(
-                ListView(id="async-list"),
-                id="list-container"
-            )
+        yield Header(show_clock=True)
+        yield Container(
+            Label("Existing Codebases", id="existing-codebases-title"),
+            ListView(id="async-list"),
+            id="list-container"
+        )
+        yield Footer()
+
+    def action_request_quit(self) -> None:
+        self.app.pop_screen()
 
     async def on_mount(self) -> None:
         await self.load_data()
@@ -35,5 +35,4 @@ class ExistingCodebaseList(ModalScreen):
         codebases_list: list = await self.query_engine_process.load_existing_codebases()
         list_items = [ListItem(Label(codebase)) for codebase in codebases_list]
         list_view.extend(list_items)
-        
-        
+
