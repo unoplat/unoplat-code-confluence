2e5667bd111d85886d87ab91aa4dde845cad4434
JayGhiya <ghiya6548@gmail.com>
2024-10-23 19:56:04 +0530

    fix: add float type to array property to hold vector indexes properly

--- unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/package_node.py
+++ unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/package_node.py
@@ -1,19 +1,22 @@
 from .base_models import BaseNode, ContainsRelationship
-from neomodel import RelationshipFrom, RelationshipTo, StringProperty,ZeroOrMore,One,ArrayProperty,VectorIndex
+from neomodel import RelationshipFrom, RelationshipTo, StringProperty,ZeroOrMore,One,ArrayProperty,VectorIndex,FloatProperty
 
 class Package(BaseNode):
     """Represents a package in the codebase"""
     package_objective = StringProperty(required=True)
     package_implementation_summary = StringProperty(required=True,default="")
+    
     package_objective_embedding = ArrayProperty(
-        default=[],
+        FloatProperty(),
         vector_index=VectorIndex(dimensions=4096, similarity_function='cosine')
     )
     package_implementation_summary_embedding = ArrayProperty(
-        default=[],
+        FloatProperty(),
         vector_index=VectorIndex(dimensions=4096, similarity_function='cosine')
     )
-    # Package relationships
+    
     codebase = RelationshipFrom('Codebase', 'CONTAINS', model=ContainsRelationship, cardinality=One)
+
     sub_packages = RelationshipTo('Package', 'CONTAINS', model=ContainsRelationship, cardinality=ZeroOrMore)
+
     classes = RelationshipTo('Class', 'CONTAINS', model=ContainsRelationship, cardinality=ZeroOrMore)
