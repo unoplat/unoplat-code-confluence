3f6f666ccc083f4ea064c856687b7221377ecc6e
JayGhiya <ghiya6548@gmail.com>
2024-10-23 19:19:10 +0530

    feat: added vector indexes and moved vector properties to sub classes to maintain distinction

--- unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/method_node.py
+++ unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/method_node.py
@@ -1,9 +1,18 @@
 from .base_models import BaseNode, ContainsRelationship
-from neomodel import RelationshipFrom, StringProperty,One
+from neomodel import RelationshipFrom, StringProperty,One,ArrayProperty,VectorIndex
 
 class Method(BaseNode):
     """Represents a method in a class"""
     function_name = StringProperty(required=True)
-    function_summary = StringProperty(default="")
+    function_implementation_summary = StringProperty(default="")
+    function_objective = StringProperty(default="")
+    function_objective_embedding = ArrayProperty(
+        default=[],
+        vector_index=VectorIndex(dimensions=4096, similarity_function='cosine')
+    )
+    function_summary_embedding = ArrayProperty(
+        default=[],
+        vector_index=VectorIndex(dimensions=4096, similarity_function='cosine')
+    )
     # Method relationships
     class_ = RelationshipFrom('Class', 'CONTAINS', model=ContainsRelationship, cardinality=One)
