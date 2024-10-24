f342c0e716ad65483a5509e31f17eca64c66e584
JayGhiya <ghiya6548@gmail.com>
2024-10-24 12:05:29 +0530

    fix: removed vector indexes and renamed properly to ensure right dimensions at the time of creation

---
+++ unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/confluence_method.py
@@ -0,0 +1,12 @@
+from .base_models import BaseNode, ContainsRelationship
+from neomodel import RelationshipFrom, StringProperty,One,ArrayProperty,VectorIndex,FloatProperty
+
+class ConfluenceMethod(BaseNode):
+    """Represents a method in a class"""
+    function_name = StringProperty(required=True)
+    function_implementation_summary = StringProperty(default="")
+    function_objective = StringProperty(default="")
+    function_objective_embedding = ArrayProperty(FloatProperty())
+    function_summary_embedding = ArrayProperty(FloatProperty())
+    # Method relationships
+    confluence_class = RelationshipFrom('ConfluenceClass', 'CONTAINS', model=ContainsRelationship, cardinality=One)
