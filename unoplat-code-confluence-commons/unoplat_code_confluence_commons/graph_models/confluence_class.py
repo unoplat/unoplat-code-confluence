393331a1f9f0ea73caed93d2663f2768bec081b5
JayGhiya <ghiya6548@gmail.com>
2024-10-24 12:05:29 +0530

    fix: removed vector indexes and renamed properly to ensure right dimensions at the time of creation

---
+++ unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/confluence_class.py
@@ -0,0 +1,13 @@
+from .base_models import BaseNode, ContainsRelationship
+from neomodel import RelationshipFrom, RelationshipTo, StringProperty,ZeroOrMore,One,ArrayProperty,VectorIndex,FloatProperty
+
+class ConfluenceClass(BaseNode):
+    """Represents a class in a package"""
+    class_name = StringProperty(required=True)
+    class_implementation_summary = StringProperty(default="")
+    class_objective = StringProperty(default="")
+    class_objective_embedding = ArrayProperty(FloatProperty())
+    class_implementation_summary_embedding = ArrayProperty(FloatProperty())
+    # Class relationships
+    package = RelationshipFrom('ConfluencePackage', 'CONTAINS', model=ContainsRelationship, cardinality=One)
+    methods = RelationshipTo('ConfluenceMethod', 'CONTAINS', model=ContainsRelationship, cardinality=ZeroOrMore)
