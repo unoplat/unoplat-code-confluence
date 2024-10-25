0ebf24bf40d54fc8784cbe7309c6906c3bb5e661
JayGhiya <ghiya6548@gmail.com>
2024-10-25 16:07:10 +0530

    fix: circular imports in models

--- unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/confluence_package.py
+++ unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/confluence_package.py
@@ -5,9 +5,8 @@ class ConfluencePackage(BaseNode):
     """Represents a package in the codebase"""
     package_objective = StringProperty(required=True)
     package_implementation_summary = StringProperty(required=True)
-    
     package_objective_embedding = ArrayProperty(FloatProperty())
     package_implementation_summary_embedding = ArrayProperty(FloatProperty())
-    confluence_codebase = RelationshipFrom('ConfluenceCodebase', 'CONTAINS', model=ContainsRelationship, cardinality=One)
-    sub_packages = RelationshipTo('ConfluencePackage', 'CONTAINS', model=ContainsRelationship, cardinality=ZeroOrMore)
-    classes = RelationshipTo('ConfluenceClass', 'CONTAINS', model=ContainsRelationship, cardinality=ZeroOrMore)
+    confluence_codebase = RelationshipFrom('.confluence_codebase.ConfluenceCodebase', 'CONTAINS', model=ContainsRelationship, cardinality=One)
+    sub_packages = RelationshipTo('.confluence_package.ConfluencePackage', 'CONTAINS', model=ContainsRelationship, cardinality=ZeroOrMore)
+    classes = RelationshipTo('.confluence_class.ConfluenceClass', 'CONTAINS', model=ContainsRelationship, cardinality=ZeroOrMore)
