c835a3d1af4fb230f4c02c96d970b11241e4d8b6
JayGhiya <ghiya6548@gmail.com>
2024-10-25 12:44:58 +0530

    fix: removed default as required is true

--- unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/confluence_package.py
+++ unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/confluence_package.py
@@ -4,7 +4,7 @@ from neomodel import RelationshipFrom, RelationshipTo, StringProperty,ZeroOrMore
 class ConfluencePackage(BaseNode):
     """Represents a package in the codebase"""
     package_objective = StringProperty(required=True)
-    package_implementation_summary = StringProperty(required=True,default="")
+    package_implementation_summary = StringProperty(required=True)
     
     package_objective_embedding = ArrayProperty(FloatProperty())
     package_implementation_summary_embedding = ArrayProperty(FloatProperty())
