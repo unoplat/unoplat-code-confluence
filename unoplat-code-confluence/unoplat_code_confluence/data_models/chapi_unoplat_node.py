add3c28afecb9b6a395075f195290f2dddb18b52
JayGhiya <ghiya6548@gmail.com>
2024-11-04 16:03:04 +0530

    feat: added more context and right exclusions for pipelines

--- unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_node.py
+++ unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_node.py
@@ -3,22 +3,22 @@ from typing import Optional, List
 from unoplat_code_confluence.data_models.chapi_unoplat_annotation import Annotation
 from unoplat_code_confluence.data_models.chapi_unoplat_class_fieldmodel import ClassFieldModel
 from unoplat_code_confluence.data_models.chapi_unoplat_import import Import
-from unoplat_code_confluence.data_models.chapi_unoplat_function import Function
+from unoplat_code_confluence.data_models.chapi_unoplat_function import ChapiUnoplatFunction
 from unoplat_code_confluence.data_models.chapi_unoplat_position import Position
 
 
 
-class Node(BaseModel):
+class ChapiUnoplatNode(BaseModel):
     node_name: Optional[str] = Field(default=None, alias="NodeName")
     type: Optional[str] = Field(default=None, alias="Type")
-    file_path: Optional[str] = Field(default=None, alias="FilePath")
-    module: Optional[str] = Field(default=None, alias="Module")
-    package: Optional[str] = Field(default=None, alias="Package")
+    file_path: Optional[str] = Field(default=None, alias="FilePath",exclude=True)
+    module: Optional[str] = Field(default=None, alias="Module",exclude=True)
+    package: Optional[str] = Field(default=None, alias="Package",exclude=True)
     multiple_extend: Optional[list[str]] = Field(default_factory=list, alias="MultipleExtend")
     fields: List[ClassFieldModel] = Field(default_factory=list, alias="Fields")
     extend: Optional[str] = Field(default=None, alias="Extend")
     imports: List[Import] = Field(default_factory=list, alias="Imports")
-    functions: List[Function] = Field(default_factory=list, alias="Functions")
-    position: Optional[Position] = Field(default=None, alias="Position")
-    content: Optional[str] = Field(default=None, alias="Content")
+    functions: List[ChapiUnoplatFunction] = Field(default_factory=list, alias="Functions")
+    position: Optional[Position] = Field(default=None, alias="Position",exclude=True)
+    content: Optional[str] = Field(default=None, alias="Content",exclude=True)
     annotations: List[Annotation] = Field(default_factory=list, alias="Annotations")
--- unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_package.py
+++ unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_package.py
@@ -1,12 +1,12 @@
 from typing import Dict, List, Optional
 from pydantic import BaseModel, Field
 
-from unoplat_code_confluence.data_models.dspy.dspy_unoplat_fs_node_subset import DspyUnoplatNodeSubset
+from unoplat_code_confluence.data_models.chapi_unoplat_node import ChapiUnoplatNode
 
 
 class UnoplatPackage(BaseModel):
     name: Optional[str] = Field(default=None,description="Name of the package")
-    node_subsets: Optional[List[DspyUnoplatNodeSubset]] = Field( default_factory=list,description="List of the node subsets for the package")
+    nodes: Optional[List[ChapiUnoplatNode]] = Field( default_factory=list,description="List of the nodes for the package")
     sub_packages: Optional[Dict[str, 'UnoplatPackage']] = Field( default_factory=dict,description="Dict of the sub-packages for the package")
 
 UnoplatPackage.model_rebuild()
