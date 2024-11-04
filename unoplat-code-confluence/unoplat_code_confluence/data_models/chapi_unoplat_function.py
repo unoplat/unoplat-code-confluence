30cb8343f2e946f2d21bf7ed902e8468e09adadb
JayGhiya <ghiya6548@gmail.com>
2024-11-04 16:03:04 +0530

    feat: added more context and right exclusions for pipelines

--- unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_function.py
+++ unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_function.py
@@ -6,12 +6,12 @@ from unoplat_code_confluence.data_models.unoplat_function_field_model import Uno
 from unoplat_code_confluence.data_models.chapi_unoplat_annotation import Annotation
 from unoplat_code_confluence.data_models.chapi_unoplat_position import Position
 
-class Function(BaseModel):
+class ChapiUnoplatFunction(BaseModel):
     name: Optional[str] = Field(default=None, alias="Name")
     return_type: Optional[str] = Field(default=None, alias="ReturnType")
     function_calls: List[FunctionCall] = Field(default_factory=list, alias="FunctionCalls")
     annotations: List[Annotation] = Field(default_factory=list, alias="Annotations")
-    position: Optional[Position] = Field(default=None, alias="Position")
+    position: Optional[Position] = Field(default=None, alias="Position",exclude=True)
     local_variables: List[UnoplatFunctionFieldModel] = Field(default_factory=list, alias="LocalVariables")
-    body_hash: Optional[int] = Field(default=None, alias="BodyHash")
+    body_hash: Optional[int] = Field(default=None, alias="BodyHash",exclude=True)
     content: Optional[str] = Field(default=None, alias="Content")
--- unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_functioncall.py
+++ unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_functioncall.py
@@ -9,9 +9,13 @@ from pydantic import BaseModel, Field
 
 
 class FunctionCall(BaseModel):
-    package: Optional[str] = Field(default=None, alias="Package")
-    type: Optional[str] = Field(default=None, alias="Type")
-    node_name: Optional[str] = Field(default=None, alias="NodeName")
-    function_name: Optional[str] = Field(default=None, alias="FunctionName")
-    parameters: List[Parameter] = Field(default_factory=list, alias="Parameters")
-    position: Optional[Position] = Field(default=None, alias="Position")
+    """
+    FunctionCall is a data model for a function call being made in a function.
+    """
+    package: Optional[str] = Field(default=None, alias="Package",description="package name of the function call")
+    type: Optional[str] = Field(default=None, alias="Type",description="type of the function call")
+    node_name: Optional[str] = Field(default=None, alias="NodeName",description="name of the node being called")
+    function_name: Optional[str] = Field(default=None, alias="FunctionName",description="name of the function being called")
+    parameters: List[Parameter] = Field(default_factory=list, alias="Parameters",description="parameters of the function call")
+    position: Optional[Position] = Field(default=None, alias="Position",exclude=True,description="position of the function call in the code")
+ 
