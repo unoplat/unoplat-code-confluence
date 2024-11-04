7ba26c6a2b6ddd664e507592de4bb987b57c52b1
JayGhiya <ghiya6548@gmail.com>
2024-11-04 16:04:40 +0530

    feat: enhanced function summary by introducing json schema

--- unoplat-code-confluence/unoplat_code_confluence/dspy_function_summary.py
+++ unoplat-code-confluence/unoplat_code_confluence/dspy_function_summary.py
@@ -1,23 +1,26 @@
 import dspy
-from unoplat_code_confluence.data_models.dspy.dspy_unoplat_fs_function_subset import DspyUnoplatFunctionSubset
-from unoplat_code_confluence.data_models.dspy.dspy_unoplat_fs_node_subset import DspyUnoplatNodeSubset
+from unoplat_code_confluence.data_models.chapi_unoplat_function import ChapiUnoplatFunction
+from unoplat_code_confluence.data_models.chapi_unoplat_node import ChapiUnoplatNode
 from loguru import logger
 
 #TODO: optimise using gpt4 judge and miprov2/textgrad
 class CodeConfluenceFunctionSummary(dspy.Signature):
     """This signature takes in metadata about function in a class and returns unoplat_function_summary with all important details."""
-    chapi_function_metadata: str = dspy.InputField(desc="This will contain function metadata regarding the function")
+    function_json_schema: str = dspy.InputField(desc="This will contain json schema of the function metadata")
+    chapi_function_metadata: str = dspy.InputField(desc="This will contain relevant function metadata regarding the function")
     unoplat_function_summary: str = dspy.OutputField(desc="This will contain function summary based on function metadata.")
 
 class CodeConfluenceFunctionCallSummary(dspy.Signature):
     """This signature takes in existing summary of function and function call metadata (one at a time) and returns enhanced unoplat_function_final_summary."""
+    function_call_json_schema: str = dspy.InputField(desc="This will contain json schema of the function call metadata")
     unoplat_function_existing_summary: str = dspy.InputField(default="Function Summary:",desc="This will contain existing function summary which can be empty during first iteration.")
-    chapi_function_call: str = dspy.InputField(desc="This will contain function call being made out of the function and its metadata.")
+    chapi_function_call: str = dspy.InputField(desc="This will contain function call being made out of the function and its relevant metadata.")
     unoplat_function_final_summary: str = dspy.OutputField(desc="Refined and restructured final summary enhanced based on existing function summary, function metadata and function call metadata.")
 
 class CodeConfluenceFunctionSummaryWithClassSignature(dspy.Signature):
     """This signature takes in chapi_class_metadata and unoplat_function_existing_summary and returns enhanced final summary unoplat_function_final_summary."""
-    chapi_class_metadata: str = dspy.InputField(desc="This will contain class metadata in json")
+    class_json_schema: str = dspy.InputField(desc="This will contain json schema of the class metadata")
+    chapi_class_metadata: str = dspy.InputField(desc="This will contain relevant class metadata in json")
     unoplat_function_existing_summary: str = dspy.InputField(desc="This will contain existing function summary without class metadata")
     unoplat_function_final_summary: str = dspy.OutputField(desc="This will contain final function summary enhanced using class metadata covering all important details")
 
@@ -35,13 +38,13 @@ class CodeConfluenceFunctionModule(dspy.Module):
         self.generate_function_summary_with_class_metadata = dspy.TypedPredictor(CodeConfluenceFunctionSummaryWithClassSignature)
         self.generate_function_objective = dspy.TypedPredictor(CodeConfluenceFunctionObjectiveSignature)
 
-    def forward(self, function_metadata: DspyUnoplatFunctionSubset, class_metadata: DspyUnoplatNodeSubset):
+    def forward(self, function_metadata: ChapiUnoplatFunction, class_metadata: ChapiUnoplatNode):
         logger.debug(f"Generating function summary for {function_metadata.name} present in class {class_metadata.node_name}")
         
-        class_subset = str(class_metadata.model_dump_json())
-        function_subset = str(function_metadata.model_dump_json()) 
+        class_subset = str(class_metadata.model_dump_json(exclude_unset=True))
+        function_subset = str(function_metadata.model_dump_json(exclude_unset=True)) 
         
-        function_summary = self.generate_function_summary(chapi_function_metadata=function_subset).unoplat_function_summary
+        function_summary = self.generate_function_summary(function_json_schema=function_subset.model_json_schema(),chapi_function_metadata=function_subset).unoplat_function_summary
        
         for function_call in function_metadata.function_calls:
             current_function_call = str(function_call.model_dump_json())
@@ -49,9 +52,9 @@ class CodeConfluenceFunctionModule(dspy.Module):
             if function_call.node_name == function_metadata.name:
                 continue
             else:
-                function_summary = self.generate_function_call_summary(unoplat_function_existing_summary=function_summary, chapi_function_call=current_function_call).unoplat_function_final_summary
+                function_summary = self.generate_function_call_summary(function_call_json_schema=current_function_call.model_json_schema(), unoplat_function_existing_summary=function_summary, chapi_function_call=current_function_call).unoplat_function_final_summary
 
-        code_confluence_function_summary = self.generate_function_summary_with_class_metadata( chapi_class_metadata=class_subset, unoplat_function_existing_summary=function_summary).unoplat_function_final_summary
+        code_confluence_function_summary = self.generate_function_summary_with_class_metadata( class_json_schema=class_subset.model_json_schema(), chapi_class_metadata=class_subset, unoplat_function_existing_summary=function_summary).unoplat_function_final_summary
 
         code_confluence_function_objective = self.generate_function_objective(function_implementation=code_confluence_function_summary).function_objective       
      
