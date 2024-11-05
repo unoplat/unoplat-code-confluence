b1d7f9b5821957876cf5b2244a68b306bf861bbf
JayGhiya <ghiya6548@gmail.com>
2024-11-05 12:02:00 +0530

    refactor: followed pydantic inheritance for data models. This will also save ton of memory

--- unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_functioncall.py
+++ unoplat-code-confluence/unoplat_code_confluence/data_models/chapi_unoplat_functioncall.py
@@ -14,7 +14,7 @@ class FunctionCall(BaseModel):
     """
     package: Optional[str] = Field(default=None, alias="Package",description="package name of the function call")
     type: Optional[str] = Field(default=None, alias="Type",description="type of the function call")
-    node_name: Optional[str] = Field(default=None, alias="NodeName",description="name of the node being called")
+    node_name: Optional[str] = Field(default=None, alias="NodeName",description="name of the class being called")
     function_name: Optional[str] = Field(default=None, alias="FunctionName",description="name of the function being called")
     parameters: List[Parameter] = Field(default_factory=list, alias="Parameters",description="parameters of the function call")
     position: Optional[Position] = Field(default=None, alias="Position",exclude=True,description="position of the function call in the code")
--- unoplat-code-confluence/unoplat_code_confluence/data_models/dspy/dspy_unoplat_function_summary.py
+++ unoplat-code-confluence/unoplat_code_confluence/data_models/dspy/dspy_unoplat_function_summary.py
@@ -1,9 +1,11 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel,Field
+from unoplat_code_confluence.data_models.chapi_unoplat_function import ChapiUnoplatFunction
 
-
-class DspyUnoplatFunctionSummary(BaseModel):
-    function_name: str = Field( alias="FunctionName", description="The name of the function")
+class DspyUnoplatFunctionSummary(ChapiUnoplatFunction):
+    qualified_name: str = Field(required=True, alias="QualifiedName",description="The qualified name of the function that contains the entire hierarchy of the class")
     objective: Optional[str] = Field(default=None, alias="Objective",description="This should include high level objective of what function does based on function content and function metadata. Should not be more than 3 lines.")
     implementation_summary: Optional[str] = Field(default=None, alias="ImplementationSummary",description="This should include implementation details of the function. make sure if this function makes internal calls to other functions of same class and to external calls to other classes/libs is also covered. Use all metadata shared for the function to answer .")
-    metadata: Optional[dict] = Field(default=None, description="Additional metadata for the function")
+    function_objective_embedding: List[float] = Field(default_factory=list, alias="FunctionObjectiveEmbedding")
+    function_implementation_summary_embedding: List[float] = Field(default_factory=list, alias="FunctionImplementationSummaryEmbedding")
+    
