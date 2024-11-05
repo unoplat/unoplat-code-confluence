8a83dfa97cfb76b3f23777f404968c6b39bc6609
JayGhiya <ghiya6548@gmail.com>
2024-11-05 12:02:00 +0530

    refactor: followed pydantic inheritance for data models. This will also save ton of memory

--- unoplat-code-confluence/unoplat_code_confluence/data_models/dspy/dspy_unoplat_node_summary.py
+++ unoplat-code-confluence/unoplat_code_confluence/data_models/dspy/dspy_unoplat_node_summary.py
@@ -1,11 +1,16 @@
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 from pydantic import BaseModel,Field
 
+from unoplat_code_confluence.data_models.chapi_unoplat_class_fieldmodel import ClassFieldModel
+from unoplat_code_confluence.data_models.chapi_unoplat_node import ChapiUnoplatNode
 from unoplat_code_confluence.data_models.dspy.dspy_unoplat_function_summary import DspyUnoplatFunctionSummary
 
-class DspyUnoplatNodeSummary(BaseModel):
-    node_name: Optional[str] = Field(default=None, alias="NodeName",description="The name of the class")
-    node_summary: Optional[str] = Field(default=None, alias="NodeSummary",description="A summary of the class")
-    node_objective: Optional[str] = Field(default=None, alias="NodeObjective",description="The objective of the class")
+class DspyUnoplatNodeSummary(ChapiUnoplatNode):
+    qualified_name: str = Field(required=True, alias="QualifiedName",description="The qualified name of the class that contains the entire hierarchy of the class")
+    node_summary: str = Field(required=True, alias="NodeSummary",description="A summary of the class")
+    node_objective: str = Field(required=True, alias="NodeObjective",description="The objective of the class")
     functions_summary: Optional[List[DspyUnoplatFunctionSummary]] = Field(default=None, alias="FunctionsSummary",description="A list of functions in the class")
-    metadata: Optional[dict] = Field(default=None, description="Additional metadata for the node")
+    class_objective_embedding: List[float] = Field(default_factory=list, alias="ClassObjectiveEmbedding")
+    class_implementation_summary_embedding: List[float] = Field(default_factory=list, alias="ClassImplementationSummaryEmbedding")
+    
+    
