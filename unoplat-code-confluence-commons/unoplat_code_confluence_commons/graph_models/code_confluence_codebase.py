8979762c6f82698dde1caf8729d8fcec129e949e
JayGhiya <ghiya6548@gmail.com>
2025-01-02 17:26:59 +0530

    feat: neomodel schema updated with newer ingestion schema

---
+++ unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/code_confluence_codebase.py
@@ -0,0 +1,47 @@
+from neomodel import (
+    StringProperty,
+    JSONProperty,
+    RelationshipTo,
+    ZeroOrMore,
+    One,
+    RelationshipFrom
+)
+from .base_models import BaseNode, ContainsRelationship
+
+class CodeConfluenceCodebase(BaseNode):
+    """
+    Represents a codebase within a Git repository.
+
+    Fields:
+        name (str): The name of the codebase or root package.
+        readme (str): Optional content of the codebase’s README file.
+        local_path (str): Filesystem path to the codebase.
+    
+    Relationships:
+        packages (RelationshipTo): Connects to package nodes.
+        package_manager_metadata (RelationshipTo): Connects to package manager metadata node.
+    """
+    name = StringProperty(required=True,unique_index=True)
+    readme = StringProperty()
+    
+    
+    packages = RelationshipTo(
+        '.code_confluence_package.CodeConfluencePackage',
+        'CONTAINS_PACKAGE',
+        model=ContainsRelationship,
+        cardinality=ZeroOrMore
+    )
+    
+    package_manager_metadata = RelationshipTo(
+        '.code_confluence_package_manager_metadata.CodeConfluencePackageManagerMetadata',
+        'HAS_PACKAGE_MANAGER_METADATA',
+        model=ContainsRelationship,
+        cardinality=One
+    )
+    
+    git_repository = RelationshipFrom(
+        '.code_confluence_git_repository.CodeConfluenceGitRepository',
+        'PART_OF_GIT_REPOSITORY',
+        model=ContainsRelationship,
+        cardinality=One
+    )
