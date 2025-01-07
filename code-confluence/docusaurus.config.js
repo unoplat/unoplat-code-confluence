78a48662ffa59035945572fd85b3643b42b04570
JayGhiya <ghiya6548@gmail.com>
2025-01-07 10:27:34 +0530

    feat: changed the url to custom domain of url

--- code-confluence/docusaurus.config.js
+++ code-confluence/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://unoplat.github.io',
+  url: 'https://docs.unoplat.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/unoplat-code-confluence/',
