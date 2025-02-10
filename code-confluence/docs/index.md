adae9091cd93c1a8b7a6eb290478c061b178e94a
JayGhiya <ghiya6548@gmail.com>
2025-02-10 16:25:39 +0530

    docs: Enhance Code Confluence Documentation Structure and Vision
    - Added comprehensive roadmap and vision documentation
    - Updated category descriptions for better navigation
    - Removed outdated index and roadmap files
    - Introduced new SVG assets for roadmap and vision pages
    - Refined footer and homepage styling
    - Improved overall documentation organization and clarity

--- code-confluence/docs/index.md
+++
@@ -1,8 +0,0 @@
----
-slug: /
-title: Documentation
----
-
-import {Redirect} from '@docusaurus/router';
-
-<Redirect to="/unoplat-code-confluence/docs/quickstart/how-to-run" />
--- code-confluence/src/components/Footer/index.js
+++ code-confluence/src/components/Footer/index.js
@@ -1,56 +1,55 @@
 import styles from './styles.module.css';
+
 export default function Footer() {
     return (
         <footer className={`footer ${styles.footerColor}`}>
-            <div class="container container--fluid">
-                <div class="row footer__links">
-                <div class="col footer__col">
-                    <h4 class="footer__title">Docs</h4>
-                    <ul class="footer__items clean-list">
-                    <li class="footer__item">
-                        <a class="footer__link-item" href="/unoplat-code-confluence/docs/">Introduction</a>
-                    </li>
-                    
-                    </ul>
+            <div className="container container--fluid">
+                <div className="row footer__links">
+                    <div className="col footer__col">
+                        <h4 className="footer__title">Docs</h4>
+                        <ul className="footer__items clean-list">
+                            <li className="footer__item">
+                                <a className="footer__link-item" href="/docs/quickstart/how-to-run">
+                                    Quick Start Guide
+                                </a>
+                            </li>
+                            <li className="footer__item">
+                                <a className="footer__link-item" href="/docs/deep-dive/vision">
+                                    Vision
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col footer__col">
+                        <h4 className="footer__title">Community</h4>
+                        <ul className="footer__items clean-list">
+                            <li className="footer__item">
+                                <a className="footer__link-item" href="https://discord.com/channels/1131597983058755675/1169968780953260106">
+                                    Discord
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col footer__col">
+                        <h4 className="footer__title">Social</h4>
+                        <ul className="footer__items clean-list">
+                            <li className="footer__item">
+                                <a className="footer__link-item" href="https://github.com/unoplat/unoplat-code-confluence">
+                                    GitHub
+                                </a>
+                            </li>
+                            <li className="footer__item">
+                                <a className="footer__link-item" href="https://x.com/unoplatio">
+                                    Twitter
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col footer__col">
-                    <h4 class="footer__title">Community</h4>
-                    <ul class="footer__items clean-list">
-                    <li class="footer__item">
-                        <a class="footer__link-item" href="https://discord.com/channels/1131597983058755675/1169968780953260106">Discord</a>
-                    </li>
-                                        
-                    </ul>
-                </div>
-                <div class="col footer__col">
-                    <h4 class="footer__title">Social</h4>
-                    <ul class="footer__items clean-list">
-                    <li class="footer__item">
-                        <a class="footer__link-item" href="https://github.com/unoplat/unoplat-code-confluence">GitHub</a>
-                    </li>
-                    <li class="footer__item">
-                        <a class="footer__link-item" href="https://x.com/unoplatio">Twitter</a>
-                    </li>
-                    </ul>
-                </div>
-                {/* <div class="col footer__col">
-                    <h4 class="footer__title">Legal</h4>
-                    <ul class="footer__items clean-list">
-                    <li class="footer__item">
-                        <a class="footer__link-item" href="#url">Privacy</a>
-                    </li>
-                    <li class="footer__item">
-                        <a class="footer__link-item" href="#url">Terms</a>
-                    </li>
-                    </ul>
-                </div> */}
-                </div>
-                <div class="text--center">
-                
-                Copyright © 2024 Unoplat Technologies, Private Limited.
+                <div className="text--center" style={{ color: 'rgba(17, 16, 17, 0.8)' }}>
+                    Copyright © {new Date().getFullYear()} Unoplat Technologies, Private Limited.
                 </div>
             </div>
         </footer>
-        )
-
+    );
 }
