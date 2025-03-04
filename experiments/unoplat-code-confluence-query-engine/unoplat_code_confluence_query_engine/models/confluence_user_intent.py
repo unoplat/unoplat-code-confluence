eb4dde839007765888a42cdecaf32372aa229d9e
JayGhiya <ghiya6548@gmail.com>
2025-03-04 11:32:08 +0530

    chore: clean up

--- experiments/unoplat-code-confluence-query-engine/unoplat_code_confluence_query_engine/models/confluence_user_intent.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum, auto
-
-
-class ConfluenceUserIntent(str, Enum):
-    CODE_SUMMARIZATION = auto()
-    PACKAGE_OVERVIEW = auto()
-    CLASS_DETAILS = auto()
-    FUNCTIONAL_IMPLEMENTATION = auto()
-
--- experiments/unoplat-code-confluence-query-engine/unoplat_code_confluence_query_engine/widgets/json_directory_tree.py
+++
@@ -1,5 +0,0 @@
-from textual.widgets import DirectoryTree
-
-class JSONDirectoryTree(DirectoryTree):
-    def filter_paths(self, paths):
-        return [path for path in paths if path.is_dir() or path.suffix.lower() == '.json']
