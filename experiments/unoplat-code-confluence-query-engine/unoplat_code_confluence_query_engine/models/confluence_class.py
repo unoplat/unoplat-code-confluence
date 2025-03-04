53cd537fb80da5c339c856d01f85f0d966995564
JayGhiya <ghiya6548@gmail.com>
2025-03-04 11:32:08 +0530

    chore: clean up

--- experiments/unoplat-code-confluence-query-engine/unoplat_code_confluence_query_engine/models/confluence_class.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class CodeConfluenceClass(BaseModel):
-    class_name: str = Field(default=None, description="The name of the class")
-    class_summary: str = Field(default=None, description="The summary of the class")
-    class_objective: str = Field(default=None, description="The objective of the class")
-    relevance_score: int = Field(default=None, description="The relevance score of the class")
--- experiments/unoplat-code-confluence-query-engine/unoplat_code_confluence_query_engine/models/confluence_package.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class CodeConfluencePackage(BaseModel):
-    package_name: str = Field(default=None, description="The name of the package")
-    package_summary: str = Field(default=None, description="The summary of the package")
-    package_objective: str = Field(default=None, description="The objective of the package")
-    relevance_score: int = Field(default=None, description="The relevance score of the package")
