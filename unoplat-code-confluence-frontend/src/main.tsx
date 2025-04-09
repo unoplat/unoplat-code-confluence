1278cefb8ae3db30fc4bcc3b6a9abc807642d304
JayGhiya <ghiya6548@gmail.com>
2025-04-09 16:37:22 +0530

    fix: remove unused import

--- unoplat-code-confluence-frontend/src/main.tsx
+++ unoplat-code-confluence-frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
