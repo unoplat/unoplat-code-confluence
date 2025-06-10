7a21258ada152850052fdb57f1d1f164aea752d7
JayGhiya <ghiya6548@gmail.com>
2025-06-10 15:59:56 +0530

    feat: schema index changes

--- unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/code_confluence_file.py
+++ unoplat-code-confluence-commons/unoplat_code_confluence_commons/graph_models/code_confluence_file.py
@@ -2,7 +2,7 @@
 from neomodel import AsyncStructuredNode, StringProperty, AsyncRelationshipTo, AsyncOne
 
 from unoplat_code_confluence_commons.graph_models.base_models import  ContainsRelationship
-from neomodel import JSONProperty, ArrayProperty
+from neomodel import JSONProperty, ArrayProperty, FulltextIndex
 
 # ⬇️  insert just above class `CodeConfluencePackage`
 class CodeConfluenceFile(AsyncStructuredNode):
@@ -14,13 +14,20 @@ class CodeConfluenceFile(AsyncStructuredNode):
     package  (PART_OF_PACKAGE)  -> CodeConfluencePackage
     """
     file_path = StringProperty(required=True, unique_index=True)
-    content   = StringProperty()
+    content   = StringProperty(fulltext_index=FulltextIndex(analyzer="english"))
     checksum  = StringProperty()
     structural_signature = JSONProperty()
     global_variables = ArrayProperty(StringProperty(), default=[])
     class_variables = JSONProperty()
-    imports = ArrayProperty(StringProperty(), default=[])
-    
+    imports = ArrayProperty(
+        StringProperty(),
+        default=[],
+        fulltext_index=FulltextIndex(analyzer="english")
+    )
+    poi_labels = ArrayProperty(
+        StringProperty(),
+        fulltext_index=FulltextIndex(analyzer="english")
+    )
     package = AsyncRelationshipTo(
         '.code_confluence_package.CodeConfluencePackage',
         'PART_OF_PACKAGE',
