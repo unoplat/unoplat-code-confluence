166621675d9cc15b3548a19ba077974a4c168c6d
JayGhiya <ghiya6548@gmail.com>
2025-12-23 16:51:46 +0530

    feat: add common links as part of sidebar

--- unoplat-code-confluence-docs/src/lib/layout.shared.tsx
+++ unoplat-code-confluence-docs/src/lib/layout.shared.tsx
@@ -1,9 +1,30 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { Linkedin } from "lucide-react";
+import DiscordIcon from "@/assets/brands/discord-icon-svgrepo-com.svg?react";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: "Unoplat Code Confluence",
     },
+    githubUrl: "https://github.com/unoplat/unoplat-code-confluence",
+    links: [
+      {
+        type: "icon",
+        text: "Discord",
+        label: "Discord Community",
+        url: "https://discord.com/channels/1131597983058755675/1169968780953260106",
+        icon: <DiscordIcon className="size-5" />,
+        external: true,
+      },
+      {
+        type: "icon",
+        text: "LinkedIn",
+        label: "LinkedIn Page",
+        url: "https://www.linkedin.com/company/unoplat/",
+        icon: <Linkedin />,
+        external: true,
+      },
+    ],
   };
 }
